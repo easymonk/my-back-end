39ce3c44a830feec199da483626bab45cbe4f0c4
easymonk <986939565@qq.com>
2024-06-20 22:05:47 +0800

    fetch去缓存

--- next-app/app/api/cron/route.js
+++ next-app/app/api/cron/route.js
@@ -3,7 +3,9 @@ import { NextResponse } from 'next/server';
 const cron_url = process.env.CRON_URL
 export async function GET () {
   console.log('hello cron');
-  const res = await fetch(`${cron_url}/api/question?s=${+new Date()}`, {
+  const res = await fetch(`${ cron_url }/api/question?s=${ +new Date() }`, {
+    next: { revalidate: 0 },
+    cache: 'no-store',
     headers: {
       // 'Content-Type': 'application/json',
     },
